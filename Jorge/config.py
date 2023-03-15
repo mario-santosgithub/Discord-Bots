121f6ab5fbba4be501288c10621463ce117c4384
Mario Santos <marioguilherme@tecnico.ulisboa.pt>
2023-03-15 13:19:54 +0000

    new branch created

---
+++ Jorge/config.py
@@ -0,0 +1,29 @@
+PREFIX = '!'
+TOKEN = 'OTY0MjEwNzE2NjI4MzYxMjM2.GromZ_.aEY3mnUaHPFS66kwxVY2VJV43-aQ56wQSlr4RI'
+CHATGPT_API_KEY = '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'
+
+status1 = 'Sou sempre eu a tratar do bot'
+status2 = 'Quero a reforma'
+status3 = 'Procuram-se Mods'
+
+rolesList = {
+    '01': 'Minecraft',
+    '02': 'LOL',
+    '03': 'Valorant',
+    '04': 'CS',
+    '05': 'Fortnite',
+    '06': 'Rocket League',
+    '07': 'Yu-gi-oh',
+    '08': 'Hearthstone',
+    '09': 'Need for Speed',
+    '10': 'FIFA',
+    '11': 'Genshin Impact',
+    '12': 'Monster Hunter',
+    '13': 'Albion',
+    '14': 'New World',
+    '15': 'Lost Ark',
+    '16': 'World of Warcraft',
+    '17': 'Stradew Valley',
+    '18': 'IPS-LEI',
+    '19': 'IST-LEIC'
+}
